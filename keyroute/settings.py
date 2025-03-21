8dd005e9e3af55e6d99974dcf0d0183b5afe0a32
codeedexprojects <codeedexprojects@gmail.com>
2025-03-21 12:17:44 +0530

    jwt

--- keyroute/settings.py
+++ keyroute/settings.py
@@ -62,6 +62,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'keyroute.urls'
 
 TEMPLATES = [
