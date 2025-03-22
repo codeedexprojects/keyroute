7fde7891899b935db4ba522c6fb4c0de115a8c0d
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-22 15:46:04 +0530

    vendor logout completed

--- vendors/urls.py
+++ vendors/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
    path('api/vendor/signup/', VendorSignupAPIView.as_view(), name='vendor-signup'),
    path('api/vendor/login/', LoginAPIView.as_view(), name='vendor_login'),
    path('api/vendor/forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
+   path('api/vendor/logout/', LogoutAPIView.as_view(), name='logout'),
+   
+
 ]
