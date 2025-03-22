c14bdeb64e05e59f5e88deaf21c26413e83464bb
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-22 16:59:33 +0530

    vendor forgot password completed with email otp

--- vendors/models.py
+++ vendors/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.utils import timezone
+import random
+from admin_panel.models import *
+
+class OTP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp_code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def generate_otp(self):
+        otp = str(random.randint(100000, 999999))  
+        self.otp_code = otp
+        self.created_at = timezone.now()
+        self.save()
+        return otp
+
+    def is_valid(self):
+        return timezone.now() - self.created_at < timezone.timedelta(minutes=5)
--- vendors/urls.py
+++ vendors/urls.py
@@ -4,8 +4,11 @@ from .views import *
 urlpatterns = [
    path('api/vendor/signup/', VendorSignupAPIView.as_view(), name='vendor-signup'),
    path('api/vendor/login/', LoginAPIView.as_view(), name='vendor_login'),
-   path('api/vendor/forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
    path('api/vendor/logout/', LogoutAPIView.as_view(), name='logout'),
+
+    path('api/vendor/send-otp/', SendOtpAPIView.as_view(), name='send_otp'),
+    path('api/vendor/verify-otp/', VerifyOtpAPIView.as_view(), name='verify_otp'),
+    path('api/vendor/reset-password/', ResetPasswordAPIView.as_view(), name='reset_password'),
    
 
 ]
