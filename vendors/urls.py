75afb41fc88246ec27b5bbf0d5d6c5c692e82585
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-22 15:17:30 +0530

    vendor forgot password completed withou otp

--- vendors/urls.py
+++ vendors/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
    path('api/vendor/signup/', VendorSignupAPIView.as_view(), name='vendor-signup'),
    path('api/vendor/login/', LoginAPIView.as_view(), name='vendor_login'),
+   path('api/vendor/forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
 ]
