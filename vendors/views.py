c45510f2e5d57c927c86acb156377b1d2e196dd4
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-22 15:17:30 +0530

    vendor forgot password completed withou otp

--- vendors/views.py
+++ vendors/views.py
@@ -5,6 +5,9 @@ from rest_framework import status
 from .serializers import VendorSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+
+from admin_panel.models import *
 
 # Create your views here.
 
@@ -50,5 +53,32 @@ class LoginAPIView(APIView):
             "refresh": str(refresh),
             "access": str(refresh.access_token),
         }, status=status.HTTP_200_OK)
+
+
+# VENDOR FORGOT PASSWORD
+class ForgotPasswordAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        new_password = request.data.get('new_password')
+        confirm_password = request.data.get('confirm_password')
+
+        if not email or not new_password or not confirm_password:
+            return Response({"error": "Email, new password, and confirm password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if new_password != confirm_password:
+            return Response({"error": "Passwords do not match."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            vendor = Vendor.objects.get(email_address=email)
+            user = vendor.user   
+            user.set_password(new_password)
+            user.save()
+            return Response({"message": "Password updated successfully!"}, status=status.HTTP_200_OK)
+
+        except Vendor.DoesNotExist:
+            return Response({"error": "Vendor with this email does not exist."}, status=status.HTTP_404_NOT_FOUND)
+
+
+
 
 
