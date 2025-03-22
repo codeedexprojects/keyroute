0a3b37d6757b144666db63e5d709e334643c2f4c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-22 14:52:24 +0530

    vendor login completed

--- vendors/views.py
+++ vendors/views.py
@@ -3,11 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import VendorSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
-
+# VENDOR REGISTRATION
 class VendorSignupAPIView(APIView):
+ 
     def post(self, request):
         serializer = VendorSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,5 +23,32 @@ class VendorSignupAPIView(APIView):
             {"errors": serializer.errors},
             status=status.HTTP_400_BAD_REQUEST
         )
+    
+# VEONDOR LOGIN
+class LoginAPIView(APIView):
+    
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response(
+                {"errors": "Username and password are required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response(
+                {"errors": "Invalid username or password."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "message": "Login successful!",
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_200_OK)
 
 
