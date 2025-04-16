a0e792dd28383adecdd501a53938e3b40ecdf829
Sahal Sabith <sahalsabith111@gmail.com>
2025-04-16 14:08:52 +0530

    Bus ANd Package Booking

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Package,Booking
+from .models import BusBooking,PackageBooking,Traveler
 
 # Register your models here.
-admin.site.register(Booking)
+admin.site.register(BusBooking)
+admin.site.register(PackageBooking)
+admin.site.register(Traveler)
