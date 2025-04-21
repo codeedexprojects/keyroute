cfef0bc7a579128f6f4acc6ff6036ae9a0ff9712
Sahal Sabith <sahalsabith111@gmail.com>
2025-04-21 10:04:39 +0530

    fix favourite issues

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -83,7 +83,7 @@ class BusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bus
         fields = [
-            'id','is_favourited',
+            'id',
             'features',
             'id',
             'features',
