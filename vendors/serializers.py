cd60d238e8aafaf4affcad063caa25cbfe6c8e33
Sahal Sabith <sahalsabith111@gmail.com>
2025-05-06 16:55:29 +0530

    package feild added in serializer

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -149,11 +149,13 @@ class BusSerializer(serializers.ModelSerializer):
         required=False
     )
 
-# <<<<<<< vendor
+    average_rating = serializers.ReadOnlyField()
+    total_reviews = serializers.ReadOnlyField()
+
     class Meta:
         model = Bus
         fields = [
-            'id', 'features', 'minimum_fare', 'bus_travel_images', 'bus_name', 'bus_number',
+            'id','average_rating', 'total_reviews', 'features', 'minimum_fare', 'bus_travel_images', 'bus_name', 'bus_number',
             'capacity', 'vehicle_description', 'vehicle_rc_number', 'travels_logo',
             'rc_certificate', 'license', 'contract_carriage_permit', 'passenger_insurance',
             'vehicle_insurance', 'bus_view_images', 'amenities', 'base_price', 'price_per_km'
@@ -521,6 +523,8 @@ class PackageSerializer(serializers.ModelSerializer):
 
     day_plans_read = DayPlanSerializer(source='dayplan_set', many=True, read_only=True)
     
+    average_rating = serializers.ReadOnlyField()
+    total_reviews = serializers.ReadOnlyField()
 
     class Meta:
         model = Package
@@ -528,7 +532,7 @@ class PackageSerializer(serializers.ModelSerializer):
             'id',
             'sub_category', 'header_image', 'places', 'days', 'nights',
             'ac_available', 'guide_included', 'buses', 
-            'day_plans','day_plans_read'
+            'day_plans','day_plans_read','average_rating', 'total_reviews','price_per_person'
         ]
 
     def validate_days(self, value):
