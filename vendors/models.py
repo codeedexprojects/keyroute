5201bb8834e56f8f26ce0a7abf8f0e0f8fb39993
Sahal Sabith <sahalsabith111@gmail.com>
2025-05-16 09:40:00 +0530

    package listing issues

--- vendors/models.py
+++ vendors/models.py
@@ -71,7 +71,6 @@ class Bus(models.Model):
     base_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     price_per_km = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     features = models.ManyToManyField(BusFeature, related_name='buses', blank=True)
-
     minimum_fare = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='available')
 
