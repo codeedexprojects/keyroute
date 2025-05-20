98838a6567e87e35bcd28f069c41b944f23f4d29
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-05-20 20:23:05 +0530

    location fixed

--- vendors/models.py
+++ vendors/models.py
@@ -159,8 +159,7 @@ class Package(models.Model):
     buses = models.ManyToManyField(Bus)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+   
     bus_location = models.CharField(max_length=255, blank=True, null=True)
     price_per_person = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     extra_charge_per_km = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
