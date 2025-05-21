827d9f0ca0a3fe7d00617c682c1b6925ca35253a
Sahal Sabith <sahalsabith111@gmail.com>
2025-05-21 14:15:15 +0530

    issues solved packages

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -857,3 +857,49 @@ class BusSerializer(serializers.ModelSerializer):
                 'description': feature.description
             } for feature in obj.features.all()
         ]
+    
+
+class ListingUserPackageSerializer(serializers.ModelSerializer):
+    package_images = PackageImageSerializer(many=True, read_only=True)
+    day_plans = DayPlanSerializer(many=True, read_only=True)
+
+    travels_name = serializers.SerializerMethodField()
+    vendor_name = serializers.CharField(source='vendor.name', read_only=True)
+    sub_category_name = serializers.CharField(source='sub_category.name', read_only=True)
+    buses = BusDetailSerializer(many=True, read_only=True)
+    is_favorite = serializers.SerializerMethodField()
+    price_per_person = serializers.SerializerMethodField()
+    extra_charge_per_km = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
+    total_reviews = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Package
+        fields = [
+            'id', 'vendor_name', 'sub_category_name', 'header_image', 'places', 'days', 'nights',
+            'ac_available', 'guide_included', 'buses', 'bus_location', 'price_per_person',
+            'extra_charge_per_km', 'status', 'average_rating', 'total_reviews',
+            'package_images', 'day_plans', 'created_at', 'updated_at', 'travels_name', 'is_favorite'
+        ]
+
+    def get_travels_name(self, obj):
+        return obj.vendor.travels_name
+
+    def get_is_favorite(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Favourite.objects.filter(user=request.user, package=obj).exists()
+        return False
+
+    def get_price_per_person(self, obj):
+        return int(obj.price_per_person)
+
+    def get_extra_charge_per_km(self, obj):
+        return int(obj.extra_charge_per_km)
+
+    def get_average_rating(self, obj):
+        avg = obj.package_reviews.aggregate(avg=Avg('rating'))['avg']
+        return round(avg, 1) if avg else 0.0
+
+    def get_total_reviews(self, obj):
+        return obj.package_reviews.count()
