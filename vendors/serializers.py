0704c8a313ccff32de619ab545e48553ad1bcad5
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-05-29 16:10:56 +0530

    admin side popular fixed

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -227,7 +227,7 @@ class BusSerializer(serializers.ModelSerializer):
             'id','average_rating', 'total_reviews', 'features', 'minimum_fare', 'bus_travel_images', 'bus_name', 'bus_number',
             'capacity', 'vehicle_description', 'vehicle_rc_number', 'travels_logo',
             'rc_certificate', 'license', 'contract_carriage_permit', 'passenger_insurance',
-            'vehicle_insurance', 'bus_view_images', 'amenities', 'base_price', 'price_per_km','location','is_favorite','bus_type','longitude','latitude','base_price_km'
+            'vehicle_insurance', 'bus_view_images', 'amenities', 'base_price', 'price_per_km','location','is_favorite','bus_type','longitude','latitude','base_price_km','is_popular'
 # =======
 #             'vehicle_insurance', 'bus_view_images', 'amenities', 'base_price', 'price_per_km','is_favorite'
 # >>>>>>> dev
