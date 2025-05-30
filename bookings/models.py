d9d58c036e5c242d349d5f292ee41c4d3af2c5c9
Sahal Sabith <sahalsabith111@gmail.com>
2025-05-30 11:43:39 +0530

    updations

--- bookings/models.py
+++ bookings/models.py
@@ -27,7 +27,7 @@ class BaseBooking(models.Model):
     )
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_date = models.DateField()
+    start_date = models.DateField(null=True,blank=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     advance_amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     
