477821c0a2b38359718126dc0b855ec788140ee9
codeedexprojects <codeedexprojects@gmail.com>
2025-06-12 11:06:48 +0530

    Update settings.py

--- keyroute/settings.py
+++ keyroute/settings.py
@@ -90,8 +90,8 @@ EMAIL_BACKEND =  "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER').strip()
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "praveen.codeedex@gmail.com "
+EMAIL_HOST_PASSWORD = "fbmq ueku gkav mygc"
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
