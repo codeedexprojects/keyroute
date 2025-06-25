d12c9cf1445e8629b0878bcddef2d2216312bd71
Sahal Sabith <sahalsabith111@gmail.com>
2025-06-25 18:10:42 +0530

    email update

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -73,7 +73,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         (USER, 'User'),
     ]
 
-    email = models.EmailField(null=True, blank=True,unique=True)
+    email = models.EmailField(null=True, blank=True)
     name = models.CharField(max_length=150)
     mobile = models.CharField(max_length=15, unique=True, null=True, blank=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=USER)
