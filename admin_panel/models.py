83fc3a461188eb5cf3e2e963111d1a5b1fbabbb4
Sahal Sabith <sahalsabith111@gmail.com>
2025-07-10 03:33:58 +0530

    fds

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -76,7 +76,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(null=True, blank=True)
     name = models.CharField(max_length=150)
     mobile = models.CharField(max_length=15, unique=True, null=True, blank=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=USER)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=VENDOR)
     is_google_user = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
