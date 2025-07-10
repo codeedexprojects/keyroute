401b5f284284d6ff75fefb2a0d4b8652d4214737
Sahal Sabith <sahalsabith111@gmail.com>
2025-07-10 12:24:50 +0530

    role added

--- keyroute/settings.py
+++ keyroute/settings.py
@@ -151,12 +151,12 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
